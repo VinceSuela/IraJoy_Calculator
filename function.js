78aa350c93e768accd088ed5d3e3e5a0e8dddd3e
Vince Suela <suela.vince.t@gmail.com>
2026-02-12 23:18:18 +0800

    fix condition

--- function.js
+++ function.js
@@ -1,17 +1,46 @@
 let screen = document.getElementById("screen");
 
 function appendToDisplay(input) {
-  const operators = ["+", "-", "*", "/", "%", "√", "."];
+  const operators = ["+", "-", "×", "÷", "%", "√", ".", "^", "!"];
   let lastChar = screen.value.slice(-1);
+  let lastTwoChars = screen.value.slice(-2);
 
-  if (screen.value === "" && operators.includes(input) && input !== "-") {
+  if (screen.value === "ERROR") {
+    screen.value = input;
     return;
   }
-  if (operators.includes(lastChar) && operators.includes(input)) {
+  if (screen.value === "" && input === ".") {
+    screen.value = "0.";
+    return;
+  }
+  if (operators.includes(lastChar) && input === ".") {
+    screen.value += "0.";
+    return;
+  }
+  if (
+    screen.value === "" &&
+    operators.includes(input) &&
+    input !== "√" &&
+    input !== "." &&
+    input !== "-"
+  ) {
+    return;
+  }
+  if (lastTwoChars === "×-" || lastTwoChars === "÷-") {
+    screen.value = screen.value.replace(lastTwoChars, input);
+    return;
+  }
+  if (input === "-" && (lastChar === "×" || lastChar === "÷")) {
+    screen.value += input;
+  } else if (operators.includes(lastChar) && operators.includes(input)) {
     screen.value = screen.value.slice(0, -1) + input;
   } else {
     screen.value += input;
   }
+}
+
+function trimTrailingOperators(expression) {
+  return expression.replace(/[+\-*/\)\)]+$/, "");
 }
 
 function calculate() {
@@ -22,6 +51,15 @@ function calculate() {
     );
 
     displayValue = displayValue.replace(/(\d+(\.\d+)?)%/g, "($1/100)");
+    displayValue = displayValue.replace(/√(\d+(\.\d+)?)/g, "Math.sqrt($1)");
+    displayValue = displayValue.replace(
+      /(\d+(\.\d+)?)\^(\d+(\.\d+)?)/g,
+      "Math.pow($1, $3)",
+    );
+    displayValue = displayValue.replace(/÷/g, "/");
+    displayValue = displayValue.replace(/×/g, "*");
+
+    displayValue = trimTrailingOperators(displayValue);
 
     let result = eval(displayValue);
 
